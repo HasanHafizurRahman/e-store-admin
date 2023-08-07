aaca37f210b8a30ef7e79ec9d607bfcff554a96d
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-08-07 21:07:14 +0600

    multiparty implemented

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -7,6 +7,7 @@ const ProductForm = ({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) => {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -32,6 +33,18 @@ const ProductForm = ({
   if (gotoproduct) {
     router.push("/products");
   }
+
+  function uploadImages(e) {
+    const files = e.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      // files.forEach((file) => data.append("file", file));
+      axios.post("/api/upload");
+    }
+  }
   return (
     <form onSubmit={saveProduct}>
       <label>Product Name</label>
@@ -41,6 +54,28 @@ const ProductForm = ({
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
+      <label>Product Image</label>
+      <div className="mb-2">
+        <label className="flex flex-col text-center border border-secondary/10 bg-primary/10 rounded-lg items-center justify-center w-24 py-2 cursor-pointer text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+        {!images?.length && <p>No images found for this product.</p>}
+      </div>
       <label>Product Description</label>
       <textarea
         placeholder="Description"
